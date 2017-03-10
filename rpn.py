9cc6237081b1e1b304f9a9783716206f5b1e3d32
Joe DiNobile <dinobes@umich.edu>
2017-03-10 11:16:08 -0500

    Add test_subtract and implement

--- rpn.py
+++ rpn.py
@@ -5,6 +5,8 @@ def calculate(arg):
 	for operand in arg.split():
 		if operand == '+':
 			stack.append(stack.pop() + stack.pop())
+		elif operand == '-':
+			stack.append((stack.pop() - stack.pop()) * -1)
 		else:
 			stack.append(float(operand))
 	return stack.pop()
